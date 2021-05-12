d6e8053adf5707f901b6df904f4c50679c446c0e
thewalkingduff <62387646+thewalkingduff@users.noreply.github.com>
2021-05-12 10:15:56 -0400

    change kombu-sign to png

--- src/components/Home/HomeElements.js
+++ src/components/Home/HomeElements.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import ImgBg from '../../images/kombu-sign.jpeg';
+import ImgBg from '../../images/kombu-sign.png';
 
 export const HomeContainer = styled.div`
   background: linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.1)),
